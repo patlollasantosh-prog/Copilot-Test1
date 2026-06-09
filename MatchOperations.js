957002d682e49bccecfcae610fae3a7fb365281a
Santosh <patlolla.santosh@gmail.com>
2026-06-09 02:47:33 -0700

    Add UI and math operations

--- MatchOperations.js
+++ MatchOperations.js
@@ -1,11 +1,27 @@
 // mathOperations.js
 
 function add(a, b) {
-    return a + b;
+    return a + b;// MatchOperations.js
+
+}
+    function subtract(a, b) {
+        return a - b;
+    }
+
+
+function multiply(a, b) {
+    return a * b;
 }
 
-function subtract(a, b) {
-    return a - b;
+function divide(a, b) {
+    if (b === 0) {
+        return 'Cannot divide by zero';
+    }
+    return a / b;
 }
 
-module.exports = { add, subtract };
+module.exports = { add, subtract, multiply, divide };
+
+
+
+
